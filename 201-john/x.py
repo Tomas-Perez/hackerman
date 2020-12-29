90607f4787bf9e59ef852a1fe3b58c380cb5eee0
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2020-12-29 12:57:06 +0100

    [201] Cleanup 8th unpack function

--- 201-john/x.py
+++ 201-john/x.py
@@ -1,6 +1,6 @@
 import math
 
-def eighth_unpack(char, other_random_number, number, debug=False):
+def eighth_unpack(char, long, debug=False):
     MAX_LONG = 9223372036854775808
     char_int = ord(char)
     calculation = math.pow(char_int, math.sqrt(char_int))
@@ -18,13 +18,9 @@ def eighth_unpack(char, other_random_number, number, debug=False):
         if debug:
             print("calculation if >= MAX_LONG:", hex(calculation))
     calculation += 0x15
+    calculation ^= long
     eax = calculation & 0xffffffff
     edx = (calculation & 0xffffffff00000000) >> 32
-    if debug:
-        print('eax:', hex(eax))
-        print('edx:', hex(edx))
-    eax ^= other_random_number
-    edx ^= number
     eax |= edx
     if debug:
         print('final eax:', hex(eax))
@@ -33,27 +29,27 @@ def eighth_unpack(char, other_random_number, number, debug=False):
     else:
         return False
 
-random_numbers_for_pos = {
-    6: (0xa66fe7dd, 0x1c),
-    7: (0x357afcf8, 0x227),
-    8: (0x00000015, 0x0),
-    9: (0x5c156c54, 0x16c),
-    10: (0xa66fe7dd, 0x1c),
-    11: (0xe93ece66, 0x9d),
-    12: (0x5c156c54, 0x16c),
-    13: (0x5c156c54, 0x16c),
-    14: (0xf3444241, 0x756),
-    15: (0x4660a4c5, 0x1),
-    16: (0xa66fe7dd, 0x1c),
-}
+random_numbers_for_pos = [
+    0x0000001ca66fe7dd,
+    0x00000227357afcf8,
+    0x0000000000000015,
+    0x0000016c5c156c54,
+    0x0000001ca66fe7dd,
+    0x0000009de93ece66,
+    0x0000016c5c156c54,
+    0x0000016c5c156c54,
+    0x00000756f3444241,
+    0x000000014660a4c5,
+    0x0000001ca66fe7dd,
+]
 
 printable_chars = [chr(i) for i in range(127)]
 
 def get_char_for_pos(pos):
     start = "A"
-    if pos in random_numbers_for_pos:
+    if pos in range(6, 17):
         for c in printable_chars:
-            if eighth_unpack(c, random_numbers_for_pos[pos][0], random_numbers_for_pos[pos][1]):
+            if eighth_unpack(c, random_numbers_for_pos[pos - 6]):
                 return c
         else:
             print('cannot solve eight_unpack for', pos)
